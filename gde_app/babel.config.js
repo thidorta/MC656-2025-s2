7d01428f07e630f8f8fa93047e22b3e8ff926fb6
Johatan Lima <johatanrlima@gmail.com>
2025-09-09 20:09:36 -0300

    Folder reordering

--- gde_app/babel.config.js
+++ gde_app/babel.config.js
@@ -1,31 +1,10 @@
 module.exports = function (api) {
   api.cache(true);
+  let plugins = [];
 
   return {
-    presets: [
-      [
-        'babel-preset-expo',
-        {
-          jsxImportSource: 'nativewind',
-        },
-      ],
-      'nativewind/babel',
-    ],
+    presets: [['babel-preset-expo', { jsxImportSource: 'nativewind' }], 'nativewind/babel'],
 
-    plugins: [
-      [
-        'module-resolver',
-        {
-          root: ['./src'],
-
-          alias: {
-            '@': './src',
-            '~': './src',
-            'tailwind.config': './tailwind.config.js',
-          },
-        },
-      ],
-      'react-native-reanimated/plugin',
-    ],
+    plugins,
   };
 };
--- gde_app/src/index.ts
+++
@@ -1,7 +0,0 @@
-// Main components
-export { ScreenContent } from './components/ScreenContent';
-export { EditScreenInfo } from './components/EditScreenInfo';
-export { Container } from './components/Container';
-
-// UI Provider
-export { GluestackUIProvider } from './components/ui/gluestack-ui-provider';
