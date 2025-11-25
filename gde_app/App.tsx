6776a60ad259cc339b6b252f8e8ebf9ecbc0d05d
mariaxmxavier <mariaxmxavier@gmail.com>
2025-11-25 04:20:28 -0300

    feat: integrate react-native-svg for enhanced SVG support

--- gde_app/App.tsx
+++ gde_app/App.tsx
@@ -1,12 +1,26 @@
 import { NavigationContainer } from '@react-navigation/native';
-import RootNavigator from './src/navigation';
 import { StatusBar } from 'expo-status-bar';
+import RootNavigator from './src/navigation';
+
+const linking = {
+  prefixes: ['http://localhost:8081', 'http://localhost:8082', 'exp://'],
+  config: {
+    screens: {
+      Welcome: '',
+      Login: 'login',
+      Home: 'home',
+      Tree: 'tree',
+      Planner: 'planner',
+      Debug: 'debug',
+    },
+  },
+};
 
 export default function App() {
   return (
     <>
       <StatusBar style="light" />
-      <NavigationContainer>
+      <NavigationContainer linking={linking} fallback={<></>}>
         <RootNavigator />
       </NavigationContainer>
     </>
